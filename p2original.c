adfa81c838e2d1199cf49b42de24e71424bf7fa7
Mahima B <97020131+BMahima@users.noreply.github.com>
2022-02-21 09:38:29 +0000

    p1

---
+++ p2original.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+void input_line(float *x1,float *y2 )
+{
+  printf("enter the coordinates: ");
+  scanf("%f %f",x1,y2);
+}
+int is_triangle(float x1,float y1, float x2, float y2,float x3, float y3)
+{
+  flaot d1,d2,d3;
+  d1==sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+  
+}
